3008019c9902359cd6e9ddd9a357847284b07c32
Tony Busch <abusch@css.edu>
2021-09-10 23:15:32 -0500

    add startGame and endGame functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,7 +5,7 @@ var playerMoney = 10
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
-var enemyAttack = 120;
+var enemyAttack = 12;
 
 var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators Round " + (i+1));
@@ -52,13 +52,38 @@ var fight = function(enemyName) {
     }
 };
 
-for(i=0; i< enemyNames.length; i++){
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    if(playerHealth > 0){
-        fight(pickedEnemyName);
-    }else{
-        window.alert("You lost in battle. GAME OVER");
-        break;
-    }
+var endGame = function(){
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+      } 
+      else {
+        window.alert("You've lost your robot in battle.");
+      }
+      var playAgainConfirm = window.confirm("Would you like to play again?");
+      if (playAgainConfirm) {
+          startGame();
+      }
+      else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+      }
 }
+
+var shop = function(){
+
+}
+
+var startGame = function(){
+    playerHealth = 100
+    playerAttack = 10
+    playerMoney = 10
+    for(i=0; i< enemyNames.length; i++){
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        if(playerHealth > 0){
+            fight(pickedEnemyName);
+        }else{;
+            break;
+        }
+    }endGame();
+}
+startGame();
