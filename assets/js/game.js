20380de2396a325caf9747269f85d339c31927a6
Tony Busch <abusch@css.edu>
2021-09-10 23:36:38 -0500

    add shop feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,7 +69,45 @@ var endGame = function(){
 }
 
 var shop = function(){
-
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
+    switch (shopOptionPrompt) {
+        case "REFILL": // new case
+        case "refill":
+          if (playerMoney >= 7) {
+            window.alert("Refilling player's health by 20 for 7 dollars.");
+      
+            playerHealth = playerHealth + 20;
+            playerMoney = playerMoney - 7;
+          }
+          else {
+            window.alert("You don't have enough money!");
+          }
+      
+          break;
+        case "UPGRADE": // new case
+        case "upgrade":
+          if (playerMoney >= 7) {
+            window.alert("Upgrading player's attack by 6 for 7 dollars.");
+      
+            playerAttack = playerAttack + 6;
+            playerMoney = playerMoney - 7;
+          }
+          else {
+            window.alert("You don't have enough money!");
+          }
+      
+          break;
+        case "LEAVE": // new case
+        case "leave":
+          window.alert("Leaving the store.");
+          break;
+        default:
+          window.alert("You did not pick a valid option. Try again.");
+          shop();
+          break;
+      }
 }
 
 var startGame = function(){
@@ -78,6 +116,12 @@ var startGame = function(){
     playerMoney = 10
     for(i=0; i< enemyNames.length; i++){
         var pickedEnemyName = enemyNames[i];
+        if (i < enemyNames.length - 1 && playerHealth > 0){
+            var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+            if (storeConfirm){
+            shop();
+            }
+        }
         enemyHealth = 50;
         if(playerHealth > 0){
             fight(pickedEnemyName);
